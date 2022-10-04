45cd8ec075c0e8c451045253043789d18c49b92e
Leonardo Hernandez <54067414+hernandez00@users.noreply.github.com>
2022-10-04 16:43:12 -0300

    gitgnore

--- _pageObjects/_locators/login.py
+++ _pageObjects/_locators/login.py
@@ -4,3 +4,18 @@ E-mail: leonardo.hernandez@fmxsolucoes.com.br
 Password: HernandeZ!#%5
 """
 
+"""
+Login field: 
+//android.view.ViewGroup[@content-desc="Campo de login"]/android.widget.EditText
+
+Password field:
+//android.view.ViewGroup[@content-desc="Campo de senha"]/android.widget.EditText
+
+Login button:
+//android.view.ViewGroup[@content-desc="Botão de envio"]
+"""
+
+from appium.webdriver.common.mobileby   import MobileBy
+
+class LoginLocators(object):
+    pass
